8f4fc777f41f98c14f2d792ea506a818a75f9168
Michaelwmx <Michaelwmx@users.noreply.github.com>
2019-12-30 21:35:32 +0800

    二分图最大匹配 匈牙利Hungarian O(n * m)

--- Bipartite_Graph_Maximum_Matching_Hungarian.cpp
+++ Bipartite_Graph_Maximum_Matching_Hungarian.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <cstring>
 #define MAXN 1005
 #define MAXM 1000005
 
@@ -18,8 +19,7 @@ void add(int x,int y){
 bool augment(int x,int stamp){
 	for(int p=head[x];p;p=e[p].next){
 		int u=e[p].to;
-		if(dfn[u] == stamp)
-			continue;
+		if(dfn[u] == stamp) continue;
 		dfn[u]=stamp;
 		if(!mat[u] || augment(mat[u],stamp)){
 			mat[u]=x;
@@ -30,11 +30,9 @@ bool augment(int x,int stamp){
 }
 
 int match(){
+	memset(mat+1,0,n2*sizeof(mat[0]));
+	memset(dfn+1,0,n2*sizeof(dfn[0]));
 	int cnt=0;
-	for(int i=1;i<=n2;i++){
-		mat[i]=0;
-		dfn[i]=0;
-	}
 	for(int i=1;i<=n1;i++){
 		if(augment(i,i))
 			cnt++;
@@ -45,8 +43,7 @@ int match(){
 int main(){
 	scanf("%d %d %d", &n1,&n2,&m);
 	tot=0;
-	for(int i=1;i<=n1;i++)
-		head[i]=0;
+	memset(head+1,0,n1*sizeof(head[0]));
 	for(int i=1;i<=m;i++){
 		int f,g;
 		scanf("%d %d", &f, &g);

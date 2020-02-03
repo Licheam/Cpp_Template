f215fecd2d031895a2e2eb70926841ef81685ac5
Michaelwmx <Michaelwmx@users.noreply.github.com>
2020-02-03 17:24:53 +0800

    快速傅立叶变换(FFT) Cooley-Tukey蝶形算法 递归实现......多项式乘法O(nlogn)

--- Fast_Fourier_Transform_Cooley-Tukey_Recursion.cpp
+++ Fast_Fourier_Transform_Cooley-Tukey_Recursion.cpp
@@ -5,25 +5,25 @@
 
 using namespace std;
 
-complex<double> omega[MAXN],arti_omega[MAXN],temp[MAXN];
+complex<double> omg[MAXN],iomg[MAXN],temp[MAXN];
 
-void init_omega(int n){
+void init(int n){
 	double PI=acos(-1);
 	for(int i=0;i<n;i++){
-		omega[i]=complex<double>(cos(2*PI*i/n),sin(2*PI*i/n));
-		arti_omega[i]=conj(omega[i]);
+		omg[i]=polar(1.0,2.0*PI*i/n);
+		iomg[i]=conj(omg[i]);
 	}
 }
 
-void FFT(int n,complex<double>* buffer,int offset,int step,complex<double>* omega){
+void FFT(int n,complex<double>* buffer,int offset,int step,complex<double>* omg){
 	if(n==1) return;
 	int m=n>>1;
-	FFT(m,buffer,offset,step<<1,omega);
-	FFT(m,buffer,offset+step,step<<1,omega);
+	FFT(m,buffer,offset,step<<1,omg);
+	FFT(m,buffer,offset+step,step<<1,omg);
 	for(int i=0;i<m;i++){
 		int pos=2*i*step;
-		temp[i]=buffer[offset+pos]+omega[i*step]*buffer[offset+step+pos];
-		temp[i+m]=buffer[offset+pos]-omega[i*step]*buffer[offset+step+pos];
+		temp[i]=buffer[offset+pos]+omg[i*step]*buffer[offset+step+pos];
+		temp[i+m]=buffer[offset+pos]-omg[i*step]*buffer[offset+step+pos];
 	}
 	for(int i=0;i<n;i++)
 		buffer[offset+i*step]=temp[i];
@@ -45,12 +45,12 @@ int main(){
 		scanf("%d", &t);
 		B[i]+=t;
 	}
-	init_omega(lim);
-	FFT(lim,A,0,1,omega);
-	FFT(lim,B,0,1,omega);
+	init(lim);
+	FFT(lim,A,0,1,omg);
+	FFT(lim,B,0,1,omg);
 	for(int i=0;i<lim;i++)
 		A[i]=A[i]*B[i];
-	FFT(lim,A,0,1,arti_omega);
+	FFT(lim,A,0,1,iomg);
 	for(int i=0;i<n+m+1;i++)
 		printf("%d ", (int)(A[i].real()/lim+0.5));
 	return 0;

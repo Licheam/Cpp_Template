dd19f1d4140b6e87a4deeb9df260d0d436f30a35
Michaelwmx <Michaelwmx@users.noreply.github.com>
2020-02-11 19:05:37 +0800

    李超线段树(P4097) 包含垂直情况

--- Li-Chao_Segment_Tree.cpp
+++ Li-Chao_Segment_Tree.cpp
@@ -1,22 +1,20 @@
 #include <cstdio>
 #include <algorithm>
-#define eps 1e-12
-#define N 50000
-#define MAXN 50005
-#define MAXT 200005
+#define N 39989
+#define MAXN 40005
+#define MAXT 160005
+const double eps=1e-12;
+const double inf=1e9;
 
 using namespace std;
 
 struct line{
-	double k,b;
 	int l,r;
-	line(){}
-	line(double _k,double _b,int _l,int _r)
-	:k(_k),b(_b),l(_l),r(_r){}
+	double k,b;
+	int id;
 }sgt[MAXT];
 
 double calc(line l,int x){return l.k*x+l.b;}
-int cross(line l1,line l2){return (int)((l1.b-l2.b)/(l2.k-l1.k));}
 
 void modify(int cur,int l,int r,line li){
 	if(li.l<=l && r<=li.r){
@@ -26,7 +24,7 @@ void modify(int cur,int l,int r,line li){
 			int mid=(l+r)>>1;
 			if(calc(li,mid)-calc(sgt[cur],mid)>eps)
 				swap(li,sgt[cur]);
-			if(cross(li,sgt[cur])-mid<-eps)
+			if(calc(li,l)-calc(sgt[cur],l)>eps)
 				modify(cur<<1,l,mid,li);
 			else modify(cur<<1|1,mid+1,r,li);
 		}
@@ -34,22 +32,26 @@ void modify(int cur,int l,int r,line li){
 	else{
 		int mid=(l+r)>>1;
 		if(li.l<=mid) modify(cur<<1,l,mid,li);
-		if(mid<li.r) modify(cur<<1|1,mid+1,r,li);
+		if(li.r>mid) modify(cur<<1|1,mid+1,r,li);
 	}
 }
 
-double query(int cur,int l,int r,int x){
-	if(l==r) return calc(sgt[cur],x);
+line query(int cur,int l,int r,int x){
+	if(l==r) return sgt[cur];
 	else{
 		int mid=(l+r)>>1;
-		if(x<=mid) return max(calc(sgt[cur],x),query(cur<<1,l,mid,x));
-		else return max(calc(sgt[cur],x),query(cur<<1|1,mid+1,r,x));
+		line t;
+		if(x<=mid) t=query(cur<<1,l,mid,x);
+		else t=query(cur<<1|1,mid+1,r,x);
+		if(!t.id || calc(sgt[cur],x)-calc(t,x)>eps) return sgt[cur];
+		else return t;
 	}
 }
 
 void built(int cur,int l,int r){
 	sgt[cur].k=sgt[cur].b=0;
 	sgt[cur].l=1; sgt[cur].r=N;
+	sgt[cur].id=0;
 	if(l<r){
 		int mid=(l+r)>>1;
 		built(cur<<1,l,mid);
@@ -62,18 +64,29 @@ int n;
 int main(){
 	scanf("%d", &n);
 	built(1,1,N);
+	int last=0,id=0;
 	for(int i=1;i<=n;i++){
-		char opt[10];
-		scanf("%s",opt);
-		if(opt[0]=='P'){
-			double s,p;
-			scanf("%lf %lf", &s, &p);
-			modify(1,1,N,line(p,s-p,1,N));
-		}
-		else{
+		int opt;
+		scanf("%d", &opt);
+		if(opt==0){
 			int x;
 			scanf("%d", &x);
-			printf("%d\n", (int)(query(1,1,N,x)/100));
+			x=(x+last-1)%N+1;
+			printf("%d\n", last=query(1,1,N,x).id);
+		}
+		else{
+			int x0,x1,y0,y1;
+			scanf("%d %d %d %d", &x0,&y0,&x1,&y1);
+			x0=(x0+last-1)%N+1;
+			x1=(x1+last-1)%N+1;
+			y0=(y0+last-1)%1000000000+1;
+			y1=(y1+last-1)%1000000000+1;
+			line t;
+			t.id=++id;
+			t.l=min(x0,x1); t.r=max(x0,x1);
+			t.k=x1==x0?0:(double)(y1-y0)/(x1-x0);
+			t.b=x1==x0?max(y0,y1):y0-t.k*x0;
+			modify(1,1,N,t);
 		}
 	}
 	return 0;

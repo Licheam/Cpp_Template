e7bf6de368d81bc99c02eb170d9150501b7b87c2
Michaelwmx <wmx_wmx@foxmail.com>
2020-03-24 21:16:31 +0800

    KMP字符串模式匹配

--- KMP.cpp
+++ KMP.cpp
@@ -8,14 +8,14 @@ char s1[MAXN],s2[MAXN];
 void kmp(){
 	next[0]=-1;
 	for(int i=1,k=-1;i<=m;i++){
-		while(k!=-1 && s2[k]!=s2[i-1])
+		while(k!=-1 && s2[k+1]!=s2[i])
 			k=next[k];
 		next[i]=++k;
 	}
 
 	cnt=0;
 	for(int i=1,k=0;i<=n;i++){
-		while(~k && s2[k]!=s1[i-1])
+		while(~k && s2[k+1]!=s1[i])
 			k=next[k];
 		if(m==++k){
 			cnt++;
@@ -25,9 +25,9 @@ void kmp(){
 }
 
 int main(){
-	scanf("%s %s", s1, s2);
-	n=strlen(s1);
-	m=strlen(s2);
+	scanf("%s %s", s1+1, s2+1);
+	n=strlen(s1+1);
+	m=strlen(s2+1);
 	kmp();
 	for(int i=1;i<=m;i++){
 		printf("%d ", next[i]);

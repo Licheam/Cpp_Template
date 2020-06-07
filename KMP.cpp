c1c695a98f22687c8719fa94decb4b350ecf786c
Michaelwmx <wmx_wmx@foxmail.com>
2020-06-07 20:59:33 +0800

    KMP字符串模式匹配

--- KMP.cpp
+++ KMP.cpp
@@ -8,13 +8,15 @@ char s1[MAXN],s2[MAXN];
 void kmp(){
 	next[0]=-1;
 	for(int i=1,k=-1;i<=m;i++){
-		while(k!=-1 && s2[k+1]!=s2[i])
+		//k初始值为-1. next数组可以查询boarder的boarder.
+		while(~k && s2[k+1]!=s2[i])
 			k=next[k];
 		next[i]=++k;
 	}
 
 	cnt=0;
 	for(int i=1,k=0;i<=n;i++){
+		//匹配串前缀与模式串后缀比，上一位前缀的boarder的boarder也能匹配。
 		while(~k && s2[k+1]!=s1[i])
 			k=next[k];
 		if(m==++k){

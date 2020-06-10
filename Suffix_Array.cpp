66eec2c345f9e4511aab1d4fc3a3a817092eaf7c
Michaelwmx <wmx_wmx@foxmail.com>
2020-06-10 23:32:56 +0800

    后缀数组 字符串排序 with Height

--- Suffix_Array.cpp
+++ Suffix_Array.cpp
@@ -4,16 +4,16 @@
 #define MAXN 1000005
 using namespace std;
 
-//sa:排名对应的前缀, rk:前缀的排名, tp:上次的rk, tax:排名对应的个数
+//sa:排名对应的前缀, rk:前缀的排名, tp:第二关键字排名对应的前缀, tax:排名对应的个数
 //height:排名i与i-1后缀的LCP(最长公共前缀)
 int sa[MAXN],rk[MAXN],tp[MAXN],tax[MAXN],height[MAXN];
 char s[MAXN];
 
 void rsort(int n,int m){
 	memset(tax,0,(m+1)*sizeof(tax[0]));
-	for(int i=1;i<=n;i++) tax[rk[i]]++;
-	for(int i=1;i<=m;i++) tax[i]+=tax[i-1];
-	for(int i=n;i>=1;i--) sa[tax[rk[tp[i]]]--]=tp[i];
+	for(int i=1;i<=n;i++) tax[rk[i]]++;//当前排名装桶
+	for(int i=1;i<=m;i++) tax[i]+=tax[i-1];//计算桶的名次
+	for(int i=n;i>=1;i--) sa[tax[rk[tp[i]]]--]=tp[i];//按照第二关键字降序，分配排名。
 }
 
 void get_sa(char* s){
@@ -24,9 +24,12 @@ void get_sa(char* s){
 	rsort(n,m);
 	for(int k=1,p;p<n;k<<=1,m=p){
 		p=0;
-		for(int i=n-k+1;i<=n;i++) tp[++p]=i;
-		for(int i=1;i<=n;i++) if(sa[i]>k) tp[++p]=sa[i]-k;
+		//重制第二关键字
+		for(int i=n-k+1;i<=n;i++) tp[++p]=i; //后续为空，排前面
+		for(int i=1;i<=n;i++) if(sa[i]>k) tp[++p]=sa[i]-k; //按照第一关键字排第二关键字
+
 		rsort(n,m);
+	
 		swap(tp,rk);
 		rk[sa[1]]=p=1;
 		for(int i=2;i<=n;i++)

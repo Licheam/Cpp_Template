c08829c3195824232d3c686b4894705b55ccf994
Michaelwmx <wmx_wmx@foxmail.com>
2020-06-17 16:58:25 +0800

    欧拉筛，线性筛筛素数，筛最小素因子,phi, mu

--- Eular_Sieve.cpp
+++ Eular_Sieve.cpp
@@ -1,29 +1,41 @@
 #include <cstdio>
 #include <cstring>
+#define MAXN 2000005
 
-#define MAXN 10000005
-#define MAXP 700005
+int cnt,p[MAXN];
+bool inp[MAXN];
+int phi[MAXN],mu[MAXN];
 
-int n,m,num,cnt,prime[MAXP],npri[MAXN];
-
-void eular_sieve(){
-	cnt=0;
-	memset(npri,0,(n+1)*sizeof(npri[0]));
-	npri[0]=npri[1]=1;
-	for(int i=2;i<=n;i++){
-		if(!npri[i]) prime[++cnt]=i;
-		for(int j=1;j<=cnt && i*prime[j]<=n;j++){
-			npri[i*prime[j]]=1;
-			if(!(i%prime[j])) break;
-		}
-	}
+void eular_sieve(int n){
+    cnt=0;
+    memset(inp,0,(n+1)*sizeof(inp[0]));
+    inp[0]=inp[1]=1;
+    phi[1]=1;
+    mu[1]=1;
+    for(int i=2;i<=n;i++){
+        if(!inp[i]) p[++cnt]=i, phi[i]=i-1, mu[i]=-1;
+        for(int j=1;j<=cnt && i*p[j]<=n;j++){
+            inp[i*p[j]]=1;
+            if(i%p[j]){
+                phi[i*p[j]]=phi[i]*(p[j]-1);
+                mu[i*p[j]]=-mu[i];
+            }
+            else{
+                phi[i*p[j]]=phi[i]*p[j];
+                mu[i*p[j]]=0;
+                break;
+            }
+        }
+    }
 }
 
 int main(){
-	scanf("%d %d", &n, &m);
-	eular_sieve();
-	for(int i=1;i<=m;i++){
-		scanf("%d",&num);
-		puts(npri[num]?"No":"Yes");
-	}
+    int n,m;
+    scanf("%d %d", &n, &m);
+    eular_sieve(n);
+    for(int i=1;i<=m;i++){
+        int k;
+        scanf("%d",&k);
+        printf("%d\n", p[k]);
+    }
 }

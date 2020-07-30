e618272162e681e7fd63130f56efa6869877ab7b
Michaelwmx <wmx_wmx@foxmail.com>
2020-07-30 09:28:24 +0800

    树状数组 with O(n) build

--- Binary_Indexed_Tree.cpp
+++ Binary_Indexed_Tree.cpp
@@ -1,13 +1,21 @@
-#include <cstdio>
-
-#define _for(i,a,b) for(int i=(a);i<=(b);i++)
-#define MAXN 500005
-
-int n,m,bit[MAXN],opt,p,q;
-
 /*
-取二进制中最低位
+	Coded with Leachim's ACM Template.
+	No errors. No warnings. ~~
 */
+#include <bits/stdc++.h>
+#pragma GCC diagnostic ignored "-Wunused-const-variable"
+#pragma GCC diagnostic ignored "-Wsign-conversion"
+#pragma GCC diagnostic ignored "-Wsign-compare"
+#define LL long long
+using namespace std;
+const int inf=0x3f3f3f3f;
+const double eps=1e-7;
+const int dx[4]={1,-1,0,0};
+const int dy[4]={0,0,1,-1};
+const int MAXN=2000005;
+
+int n,m,bit[MAXN];
+
 int lowbit(int x){
 	return x&(-x);
 }
@@ -24,48 +32,33 @@ int sum(int x){
 	return s;
 }
 
-int main(){
+void build() {
+    for(int x=1;x<<1<=n;x<<=1)
+        for(int i=x;i+x<=n;i+=x<<1)
+            bit[i+x]+=bit[i];
+}
+
+void solve() {
 	scanf("%d %d", &n, &m);
-	_for(i,1,n+1)
-		bit[i]=0;
-	q=0;
-	_for(i,1,n){
-		scanf("%d", &p);
-		change(i,p-q);
-		q=p;
-	}
-	_for(i,1,m){
-		int le,ri;
-		scanf("%d", &opt);
-		if(opt==1){
-			scanf("%d %d %d",&le,&ri,&p);
-			change(le,p);
-			change(ri+1,-p);
-		}
-		else{
-			scanf("%d", &p);
-			printf("%d\n", sum(p));
+	for(int i=1;i<=n;i++) scanf("%d", &bit[i]);
+	build();
+	for(int i=1;i<=m;i++) {
+		int opt,x,y;
+		scanf("%d %d %d", &opt, &x, &y);
+		if(opt==1) {
+			change(x,y);
+		} else {
+			printf("%d\n", sum(y)-sum(x-1));
 		}
 	}
 }
 
-/*
-单点修改 区间查询
-int main(){
-	scanf("%d %d", &n, &m);
-	_for(i,1,n)
-		bit[i]=0;
-	_for(i,1,n){
-		scanf("%d",&q);
-		change(i,q);
-	}
-	_for(i,1,m){
-		scanf("%d %d %d",&opt, &p,&q);
-		if(opt==1)
-			change(p,q);
-		else
-			printf("%d\n", sum(q)-sum(p-1));
+int main() {
+	int T=1,cas=1;(void)(cas);
+	// scanf("%d", &T);
+	while(T--) {
+		// printf("Case #%d: ", cas++);
+		solve();
 	}
 	return 0;
 }
-*/
